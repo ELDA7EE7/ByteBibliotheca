7951aeeb2939eb6c1c42c84f9a2b22d0d05cef5d
ELDA7EE7 <mazenmohamedabd@gmail.com>
2023-12-01 20:31:00 +0200

    user registeration brought to you by ELDA7EE7 & mazen alaa

--- Libirary_/src/main/java/com/example/libirary_/CreateNewUserAccountSceneController.java
+++ Libirary_/src/main/java/com/example/libirary_/CreateNewUserAccountSceneController.java
@@ -1,4 +1,58 @@
 package com.example.libirary_;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+
 public class CreateNewUserAccountSceneController {
+    @FXML
+    private TextField
+            usernameTextField,
+            emailTextField;
+    @FXML
+    private PasswordField enterPasswordField,
+            confirmPasswordField;
+    private String username,
+            email,
+            password,
+            confirmPassword;
+    @FXML
+    private Label confirmpasswordLabel;
+    @FXML
+    private Button signupButton;
+
+    public void getUsername(ActionEvent event){
+        username= usernameTextField.getText();
+        System.out.println(username);
+    }
+    public void getPassword(ActionEvent event){
+        if(username == null)
+            usernameTextField.setText("");
+        password= enterPasswordField.getText();
+        System.out.println(password);
+
+    }
+    public void getEmail(ActionEvent event){
+        email= emailTextField.getText();
+        System.out.println(email);
+    }
+    public void ConfirmPassword(ActionEvent event){
+        confirmPassword= confirmPasswordField.getText();
+        System.out.println(confirmPassword);
+        if (!password.equals(confirmPassword)){
+            confirmpasswordLabel.setText("Password does not match");
+            confirmPasswordField.clear();
+        }
+        else {
+            confirmpasswordLabel.setText("");
+        }
+    }
+    public void SignUp(ActionEvent event) {
+
+        User newuser = new User(username,email,password);
+
+    }
 }
--- Libirary_/src/main/java/com/example/libirary_/User.java
+++ Libirary_/src/main/java/com/example/libirary_/User.java
@@ -1,18 +1,21 @@
 package com.example.libirary_;
 
+import java.util.ArrayList;
+
 public class User {
     static int userCounter =0;
     final int userID;
     String userName;
     String email;
     String password;
-
+    static ArrayList<User> users = new ArrayList<User>();
     public User(String userName, String email, String password) {
         userCounter++;
         this.userID = userCounter+200;
         this.userName = userName;
         this.email = email;
         this.password = password;
+        users.add(this);
     }
 
     public String getEmail() {
