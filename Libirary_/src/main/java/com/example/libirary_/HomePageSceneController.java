9d9a8ef18b89796f491f2abdd81edc79ba7826df
Mario <mario4maged.mm@gmail.com>
2023-12-05 01:57:25 +0200

    version 3

--- Libirary_/src/main/java/com/example/libirary_/HomePageSceneController.java
+++ Libirary_/src/main/java/com/example/libirary_/HomePageSceneController.java
@@ -1,6 +1,38 @@
 package com.example.libirary_;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
 public class HomePageSceneController
 {
+    private Stage stage;
+    private Scene scene;
+    private Parent root;
+    public void RandomBookGen(ActionEvent e) throws IOException {
+        root= FXMLLoader.load(getClass().getResource("RandomPage.fxml"));
+        stage=(Stage) ((Node) e.getSource()).getScene().getWindow();
+        scene= new Scene(root);
+        stage.setScene(scene);
+
+        stage.show();
+
+    }
+
+    public void cart(ActionEvent e) throws IOException {
+        root= FXMLLoader.load(getClass().getResource("CartScene.fxml"));
+        stage=(Stage) ((Node) e.getSource()).getScene().getWindow();
+        scene= new Scene(root);
+        stage.setScene(scene);
+        stage.show();
+
+    }
+
 
 }
--- Libirary_/src/main/java/com/example/libirary_/RandomBook.java
+++ Libirary_/src/main/java/com/example/libirary_/RandomBook.java
@@ -15,14 +15,6 @@ public class RandomBook
     private Stage stage;
     private Scene scene;
     private Parent root;
-    public void RandomBookGen(ActionEvent e) throws IOException {
-        root= FXMLLoader.load(getClass().getResource("RandomPage.fxml"));
-        stage=(Stage) ((Node) e.getSource()).getScene().getWindow();
-        scene= new Scene(root);
-        stage.setScene(scene);
-        stage.show();
-
-    }
     public void BackRandom(ActionEvent e) throws IOException {
         root= FXMLLoader.load(getClass().getResource("HomePageScene.fxml"));
         stage=(Stage) ((Node) e.getSource()).getScene().getWindow();
