65371f1f1229a91bc77d31548a8bdaa3d32000a1
Mugetsu276 <loayelhattab@yahoo.com>
2023-12-18 01:35:19 +0200

    Shopping Cart GUI Front 95% complete

--- Libirary_/src/main/java/com/example/libirary_/ShoppingCartController.java
+++ Libirary_/src/main/java/com/example/libirary_/ShoppingCartController.java
@@ -5,32 +5,57 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.RadioButton;
+import javafx.scene.control.TextField;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.input.MouseEvent;
 import javafx.stage.Stage;
 import resourcesimports.UserInterfaceIcons;
+
 import java.io.IOException;
 
 public class ShoppingCartController {
     private Stage stage;
     private Scene scene;
     private Parent root;
+
     @FXML
-    ImageView backIcon;
+    private Button Categories_id;
+
     @FXML
-    ImageView profileIcon;
+    private ImageView backIcon;
+
+    @FXML
+    private RadioButton cashOnDelivery;
+
+    @FXML
+    private RadioButton credit;
+
+    @FXML
+    private ImageView profileIcon;
+
+    @FXML
+    private TextField promoCodeField;
+
+    @FXML
+    private Label totalPrice;
     Image backIconOnHover = UserInterfaceIcons.goBackOnHover;
     Image backIconOnClick = UserInterfaceIcons.goBackOnClick;
     Image profileIconOnHover = UserInterfaceIcons.profileOnHover;
     Image profileIconOnClick = UserInterfaceIcons.profileOnClick;
+
     @FXML
-    public void switchBackIconOnHover (){
+    public void switchBackIconOnHover() {
         backIcon.setImage(backIconOnHover);
     }
-    public void switchProfileIconOnHover (){
+
+    public void switchProfileIconOnHover() {
         backIcon.setImage(profileIconOnHover);
     }
+
     @FXML
     void backToHomePage(MouseEvent event) throws IOException {
         backIcon.setImage(backIconOnClick);
@@ -41,6 +66,7 @@ public class ShoppingCartController {
         stage.setFullScreen(true);
         stage.show();
     }
+
     @FXML
     void goToProfilePage(MouseEvent event) throws IOException {
         profileIcon.setImage(profileIconOnClick);
--- Libirary_/src/main/java/shoppingcart/ShoppingCart.java
+++ Libirary_/src/main/java/shoppingcart/ShoppingCart.java
@@ -11,57 +11,73 @@ public class ShoppingCart {
     private String paymentMethod;
     private float totalPrice;
     private float discountAmount;
+
     public List<Book> getBooks() {
         return books;
     }
+
     public void setBooks(List<Book> books) {
         this.books = books;
     }
+
     public String getPaymentMethod() {
         return paymentMethod;
     }
+
     public void setPaymentMethod(String paymentMethod) {
         this.paymentMethod = paymentMethod;
     }
+
     public float getTotalPrice() {
         return totalPrice;
     }
+
     public void setTotalPrice(float totalPrice) {
         this.totalPrice = totalPrice;
     }
+
     public List<Discount> getDiscounts() {
         return discounts;
     }
+
     public void setDiscounts(List<Discount> discounts) {
         this.discounts = discounts;
     }
+
     public float getDiscountAmount() {
         return discountAmount;
     }
+
     public void setDiscountAmount(float discountAmount) {
         this.discountAmount = discountAmount;
     }
-    public void addBook(Book book){
+
+    public void addBook(Book book) {
         BookAdder bookAdder = new BookAdder(book);
         bookAdder.execute(this);
     }
-    public void removeBook(Book book){
+
+    public void removeBook(Book book) {
         BookRemover bookRemover = new BookRemover(book);
         bookRemover.execute(this);
     }
-    public void determinePaymentMethod(boolean paymentIsCash){
+
+    public void determinePaymentMethod(boolean paymentIsCash) {
         PaymentMethodDeterminer paymentMethodDeterminer = new PaymentMethodDeterminer(paymentIsCash);
         paymentMethodDeterminer.execute(this);
     }
-    public void updateQuantity(Book book, int updatedQuantity){
+
+    public void updateQuantity(Book book, int updatedQuantity) {
         QuantityUpdater quantityUpdater = new QuantityUpdater(book, updatedQuantity);
         quantityUpdater.execute(this);
     }
-    public void calculateBookPrice(Book book, int updatedQuantity){
+
+    public void calculateBookPrice() {
         BookPriceCalculator bookPriceCalculator = new BookPriceCalculator();
         bookPriceCalculator.execute(this);
     }
-    public void calculateTotalPrice(){
+
+    public void calculateTotalPrice() {
         TotalPriceCalculator totalPriceCalculator = new TotalPriceCalculator();
         totalPriceCalculator.execute(this);
     }
