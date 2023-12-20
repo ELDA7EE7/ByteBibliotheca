f2dd6c21740fcc9a57e3fa3396a3ccc75fe1cd78
mazen <mazen15408@gmail.com>
2023-12-20 19:50:45 +0200

    updateSearch&SolveBugs

--- Libirary_/src/main/java/com/example/libirary_/SearchSceneController.java
+++ Libirary_/src/main/java/com/example/libirary_/SearchSceneController.java
@@ -1,6 +1,6 @@
 package com.example.libirary_;
 
-import librarypackage.Library;
+import searchengine.FillLists;
 import searchengine.SearchEngine;
 import javafx.beans.value.ChangeListener;
 import javafx.beans.value.ObservableValue;
@@ -17,42 +17,88 @@ import javafx.stage.Stage;
 
 import java.io.IOException;
 import java.net.URL;
+import java.util.ArrayList;
 import java.util.ResourceBundle;
 
+import static com.example.libirary_.MainClass.booksTrie;
 import static librarypackage.Library.setSelectedBook;
 
-public class SearchSceneController extends SearchEngine implements Initializable  {
+public class SearchSceneController extends FillLists implements SearchEngine , Initializable  {
+
+    protected ArrayList<String> words = new ArrayList<>();
     @FXML
     private TextField searchBar;
     @FXML
-    private ListView<String> listView;
+    private ListView<String> searchListView;
+
+
     @Override
     public void initialize(URL arg0, ResourceBundle arg1) {
-        super.uploadAllBooks();
-        listView.getItems().addAll(words);
+        uploadAllBooks(words);
+        searchListView.getItems().addAll(words);
+        uploadAvailableBooks();
         // do event when click on any word
         try {
-        listView.getSelectionModel().selectedItemProperty().addListener(new ChangeListener<String>() {
+        searchListView.getSelectionModel().selectedItemProperty().addListener(new ChangeListener<String>() {
             @Override
             public void changed(ObservableValue<? extends String> arg0, String arg1, String arg2) {
-                String selectedWord = listView.getSelectionModel().getSelectedItem();
-                setSelectedBook(findBook(selectedWord));
-                try {
-                    switchToBookDetailsScene(searchBar);
-                } catch (IOException e) {
-                    throw new RuntimeException(e);
+                String selectedWord = searchListView.getSelectionModel().getSelectedItem();
+                if(booksTrie.search(selectedWord)) {
+                    setSelectedBook(findBook(selectedWord));
+                    try {
+                        switchToBookDetailsScene(searchBar);
+                    } catch (IOException e) {
+                        throw new RuntimeException(e);
+                    }
                 }
+               else
+                    uploadAuthorBooks(selectedWord);
+
             }
         });
         }catch (NullPointerException ex){
         System.out.println(ex);
         }
+
+        try {
+            availableList.getSelectionModel().selectedItemProperty().addListener(new ChangeListener<String>() {
+                @Override
+                public void changed(ObservableValue<? extends String> arg0, String arg1, String arg2) {
+                    String selectedWord = availableList.getSelectionModel().getSelectedItem();
+                    setSelectedBook(findBook(selectedWord));
+                    try {
+                        switchToBookDetailsScene(searchBar);
+                    } catch (IOException e) {
+                        throw new RuntimeException(e);
+                    }
+                }
+            });
+        }catch (NullPointerException ex){
+            System.out.println(ex);
+        }
+
+        try {
+            authorList.getSelectionModel().selectedItemProperty().addListener(new ChangeListener<String>() {
+                @Override
+                public void changed(ObservableValue<? extends String> arg0, String arg1, String arg2) {
+                    String selectedWord = authorList.getSelectionModel().getSelectedItem();
+                    setSelectedBook(findBook(selectedWord));
+                    try {
+                        switchToBookDetailsScene(searchBar);
+                    } catch (IOException e) {
+                        throw new RuntimeException(e);
+                    }
+                }
+            });
+        }catch (NullPointerException ex){
+            System.out.println(ex);
+        }
     }
 
     @FXML//when click on search button
     void search(ActionEvent event) {
-        listView.getItems().clear();
-        listView.getItems().addAll(super.searchList(searchBar.getText(),words));
+        searchListView.getItems().clear();
+        searchListView.getItems().addAll(searchList(searchBar.getText(),words));
     }
     @FXML
     void BackToHomePage(ActionEvent event) throws IOException {
--- Libirary_/src/main/java/searchengine/SearchEngine.java
+++ Libirary_/src/main/java/searchengine/SearchEngine.java
@@ -9,25 +9,25 @@ import java.util.stream.Collectors;
 
 import static librarypackage.Library.books;
 
-public abstract class SearchEngine {
-   protected ArrayList<String> words = new ArrayList<>();
+public  interface SearchEngine {
 
-    protected void uploadAllBooks (){
+
+ default void uploadAllBooks (ArrayList<String> words){
         for (Book book: books) {
             words.add(book.getTitle());
             words.add(book.getAuthor());
         }
     }
 
-    public Book findBook(String word){
+    default Book findBook(String word){
         for(Book book: books ){
-            if(word.equals(book.getAuthor())||word.equals(book.getTitle()))
+            if(word.equals(book.getTitle()))
                 return book;
         }
         return null;
     }
     //this function returns all words that contains String you write in search textField
-    protected List<String> searchList(String searchWords, List<String> listOfStrings) {
+    default List<String> searchList(String searchWords, List<String> listOfStrings) {
         List<String> searchWordsArray = Arrays.asList(searchWords.trim().split(" "));
         return listOfStrings.stream().filter(input -> {
             return searchWordsArray.stream().allMatch(word ->
