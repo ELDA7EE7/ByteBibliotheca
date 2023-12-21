fd7247557bd14263b9e639f87d02fca8ac814e26
Mugetsu276 <loayelhattab@yahoo.com>
2023-12-21 05:32:01 +0200

    Search Scene GUI

--- Libirary_/src/main/java/com/example/libirary_/SearchSceneController.java
+++ Libirary_/src/main/java/com/example/libirary_/SearchSceneController.java
@@ -1,5 +1,9 @@
 package com.example.libirary_;
 
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.input.MouseEvent;
+import resourcesimports.UserInterfaceIcons;
 import searchengine.FillLists;
 import searchengine.SearchEngine;
 import javafx.beans.value.ChangeListener;
@@ -24,14 +28,80 @@ import static com.example.libirary_.MainClass.booksTrie;
 import static librarypackage.Library.setSelectedBook;
 
 public class SearchSceneController extends FillLists implements SearchEngine , Initializable  {
-
+    private Stage stage;
+    private Scene scene;
+    private Parent root;
+    @FXML
+    public ImageView backIcon,profileIcon,shoppingCartIcon;
+    private final Image backIconDefault = UserInterfaceIcons.goBack;
+    private final Image backIconOnHover = UserInterfaceIcons.goBackOnHover;
+    private final Image backIconOnClick = UserInterfaceIcons.goBackOnClick;
+    private final Image profileIconDefault = UserInterfaceIcons.profile;
+    private final Image profileIconOnHover = UserInterfaceIcons.profileOnHover;
+    private final Image profileIconOnClick = UserInterfaceIcons.profileOnClick;
+    public Image shoppingCartIconDefault = UserInterfaceIcons.shoppingCart;
+    public Image shoppingCartIconOnHover = UserInterfaceIcons.shoppingCartOnHover;
+    public Image shoppingCartIconOnClick = UserInterfaceIcons.shoppingCartOnClick;
     protected ArrayList<String> words = new ArrayList<>();
     @FXML
     private TextField searchBar;
     @FXML
     private ListView<String> searchListView;
 
-
+    @FXML
+    void switchBackIconOnHover(MouseEvent event) {
+        backIcon.setImage(backIconOnHover);
+    }
+    @FXML
+    void resetBackIcon(){
+        backIcon.setImage(backIconDefault);
+    }
+    @FXML
+    void switchProfileIconOnHover(MouseEvent event) {
+        profileIcon.setImage(profileIconOnHover);
+    }
+    @FXML
+    void resetProfileIcon(){
+        profileIcon.setImage(profileIconDefault);
+    }
+    @FXML
+    void switchShoppingCartIconToHover(){
+        shoppingCartIcon.setImage(shoppingCartIconOnHover);
+    }
+    @FXML
+    void resetShoppingCartIcon(){
+        shoppingCartIcon.setImage(shoppingCartIconDefault);
+    }
+    @FXML
+    void switchToShoppingCart(MouseEvent e) throws IOException{
+        shoppingCartIcon.setImage(shoppingCartIconOnClick);
+        root= FXMLLoader.load(getClass().getResource("ShoppingCart.fxml"));
+        stage=(Stage) ((Node) e.getSource()).getScene().getWindow();
+        scene= new Scene(root);
+        stage.setFullScreen(true);
+        stage.setScene(scene);
+        stage.show();
+    }
+    @FXML
+    void switchToUserProfile(MouseEvent e) throws IOException {
+        profileIcon.setImage(profileIconOnClick);
+        root= FXMLLoader.load(getClass().getResource("UserProfile.fxml"));
+        stage=(Stage) ((Node) e.getSource()).getScene().getWindow();
+        scene= new Scene(root);
+        stage.setFullScreen(true);
+        stage.setScene(scene);
+        stage.show();
+    }
+    @FXML
+    void backToHomePage(MouseEvent event) throws IOException {
+        backIcon.setImage(backIconOnClick);
+        root = FXMLLoader.load(getClass().getResource("HomePage.fxml"));
+        stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+        scene = new Scene(root);
+        stage.setScene(scene);
+        stage.setFullScreen(true);
+        stage.show();
+    }
     @Override
     public void initialize(URL arg0, ResourceBundle arg1) {
         uploadAllBooks(words);
@@ -101,15 +171,6 @@ public class SearchSceneController extends FillLists implements SearchEngine , I
     void search(ActionEvent event) {
         searchListView.getItems().clear();
         searchListView.getItems().addAll(searchList(searchBar.getText(),words));
-    }
-    @FXML
-    void BackToHomePage(ActionEvent event) throws IOException {
-        Parent root = FXMLLoader.load(getClass().getResource("HomePage.fxml"));
-        Stage stage = (Stage) ((Node)event.getSource()).getScene().getWindow();
-        Scene scene = new Scene(root);
-        stage.setScene(scene);
-        stage.setFullScreen(true);
-        stage.show();
     }
 
     void switchToBookDetailsScene(Node node) throws IOException {
